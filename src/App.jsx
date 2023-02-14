51df19cc5893bd70921a8f9afeba376fa29f7453
Prachi Gore <prachigore408@gmail.com>
2023-02-14 21:48:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-
-
-import './App.css'
+import React from 'react';
+import styled from 'styled-components';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Login from './Components/Login';
 
 function App() {
  
 
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <Router>
+    <Container>
+    <img src="" alt='logo is loading...'></img>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<h1>SignUp Page</h1>}></Route>
+      
+      </Routes>
+     </Container>
+     </Router>
   )
 }
-
+const Container=styled.div`
+width:100vw;
+color:red;
+`;
 export default App
