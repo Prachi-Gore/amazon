ba06ccd9438e9239365ce2fa18167c5bca327ee3
Prachi Gore <prachigore408@gmail.com>
2023-02-14 21:48:51 +0530

    Create Login.jsx

---
+++ src/Components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+const Login = () => {
+  return (
+    <Container>
+      <h1>WELCOME TO LOGIN PAGE </h1>
+      </Container>
+  )
+}
+const Container=styled.div`
+width:100vw;
+color:green;
+`
+export default Login;
