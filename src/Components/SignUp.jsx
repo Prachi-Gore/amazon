02bd5364e4689ea58357cb47aec325147718a2e1
Prachi Gore <prachigore408@gmail.com>
2023-02-23 11:55:06 +0530

    Create SignUp.jsx

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const SignUp=()=>{
+    const logoPath="/amazon_logo.png";
+    return(
+    <Container>
+     <Logo >
+      <img src={logoPath} alt='logo is loading...'></img>
+    </Logo>
+    <Form>
+        <h3>Sign-Up</h3>
+        <label for="name">Name</label>
+        <input type="text"placeholder='Prachi Gore' id="name"/>
+        <label for="mail">Email</label>
+        <input type="email"placeholder='example@.com' id="mail"/>
+        <label for="password">Password</label>
+        <input type="password"placeholder='********'/>
+         < SignupButton>Create Account in Amazon</SignupButton>
+        <InfoText>By continuing, you agree to Amazon's <span>Condtions of Use </span>and <span>Privacy Notice </span>.</InfoText>
+      </Form>
+      <LoginButton>Back to Login</LoginButton>
+    
+      </Container>
+    )
+}
+const Container=styled.div`
+width:100vw;
+display:flex;
+flex-direction:column ;
+align-items: center;
+`;
+
+const Logo=styled.div`
+width:170px;
+padding:20px;
+img{
+  width:100%; 
+}
+`;
+const Form=styled.form`
+display:flex ;
+flex-direction: column;
+width:30%;
+height:500px ;
+padding:20px ;
+border:solid 1.5px lightgrey ;
+h3{
+  font-size: 30px;
+  font-weight: 400 ;
+  align-self: flex-start;
+  margin-bottom: 20px;
+}
+label{
+font-size: 20px;
+font-weight:600 ;
+margin-bottom:6px ;
+}
+input{
+font-size:16px ;
+padding:5px ;
+margin-bottom:20px;
+width:90% ;
+border: solid 1px lightgray;
+border-radius:5px ;
+&:hover{
+  border-color:black; 
+}
+}
+`;
+const LoginButton=styled.button`
+width:25%;
+border: solid 1px lightgray;
+border-radius:10px ;
+font-size: 17px;
+background-color:#f3b414;
+padding: 7px;
+margin-top: 25px;
+justify-content: center;
+`;
+const InfoText=styled.p`
+margin-top: 25px;
+font-size: 16px;
+width: 100%;
+word-wrap: normal;
+span{
+  color:#426bc0;
+}
+`;
+const SignupButton=styled.button`
+width:90%;
+height:35px;
+font-size: 15px;
+margin-top: 20px;
+&:hover{
+  background-color:#dfdfdf;
+  border: 1px solid gray;
+}
+`;
+
+export default SignUp;
