711c2e8b420e8b194f344904044666896d54c799
Prachi Gore <prachigore408@gmail.com>
2023-02-24 00:20:02 +0530

    Update reducer.jsx

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -5,7 +5,7 @@ export const initialState={
     user:null,
     
 }
-    
+export const getBasketTotal=(basket)=>basket.reduce((amount,item)=>item.price+amount,0) 
 const reducer=(state,action)=>{
     //console.log("action=>",action)
     
@@ -15,10 +15,22 @@ const reducer=(state,action)=>{
             return( {
                 ...state,
                 basket:[...state.basket,action.item]
-            }
-            
+            } 
             )
-                
+        case 'REMOVE_FROM_BASKET'  :
+            const index=state.basket.findIndex(
+                (basketItem)=>basketItem.id===action.id
+            ) ;
+            let newBasket=[...state.basket] ;
+            if(index>=0){
+                newBasket.splice(index,1)
+            }else{
+                console.warn(`can't remove product whose index is ${index}`)
+            }
+            return{
+                ...state,
+                basket:newBasket
+            }    
         default:
                 return state;
     }
